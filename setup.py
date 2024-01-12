0e3856c1be87632883c82f35449d6342471b5b77
vasalosi <98632541+vasalosi@users.noreply.github.com>
2024-01-12 10:33:28 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'EmergencyDetection',
+    version= '0.0.0',
+    author= 'Ioannis Vasalos',
+    author_email= 'vasalosi@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
