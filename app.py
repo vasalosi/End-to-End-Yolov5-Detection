c93cf601136dc3ef5dda45ae78e1c22a56c55ff7
vasalosi <98632541+vasalosi@users.noreply.github.com>
2024-01-26 10:06:13 +0200

    Modified

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
 from EmergencyDetection.pipeline.training_pipeline import TrainPipeline
 
-<<<<<<< HEAD
 obj = TrainPipeline()
-#obj = TrainPipeline.run_pipeline()
-=======
-#obj = TrainPipeline()
-obj = TrainPipeline.run_pipeline()
->>>>>>> 6e19a02c6834c7a44b518c1656e597782d7e0cc4
